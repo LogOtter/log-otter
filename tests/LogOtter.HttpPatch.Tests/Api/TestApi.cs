6f4135adda50851dce8bffecae688f4d0b4e3a7b
Andrew Butler <andrew.butler@ensono.com>
2022-11-25 12:29:34 +0000

    Refactor HttpPatch tests

--- tests/LogOtter.HttpPatch.Tests/Api/TestApi.cs
+++ tests/LogOtter.HttpPatch.Tests/Api/TestApi.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace LogOtter.HttpPatch.Tests.Api;
 
