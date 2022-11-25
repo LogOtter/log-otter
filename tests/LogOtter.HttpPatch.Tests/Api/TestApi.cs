c95f783cc0b7b11bc75f50d6e1997f16c46dbd16
Andrew Butler <1628649+AButler@users.noreply.github.com>
2022-11-25 12:26:31 +0000

    Refactor HttpPatch tests

--- tests/LogOtter.HttpPatch.Tests/Api/TestApi.cs
+++ tests/LogOtter.HttpPatch.Tests/Api/TestApi.cs
@@ -1,8 +1,5 @@
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace LogOtter.HttpPatch.Tests.Api;
 
