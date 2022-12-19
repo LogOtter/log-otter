587e604679dfcc81854211f0a735df18bf9f3497
Andrew Butler <butlerat@gmail.com>
2022-12-19 13:24:24 +0000

    Update NuGet dependencies (#7)

--- src/LogOtter.CosmosDb.EventStore/EventStreamApi/EventStreamsApiOptions.cs
+++ src/LogOtter.CosmosDb.EventStore/EventStreamApi/EventStreamsApiOptions.cs
@@ -1,13 +1,11 @@
-﻿using Microsoft.Extensions.Primitives;
-
-namespace LogOtter.CosmosDb.EventStore.EventStreamApi;
+﻿namespace LogOtter.CosmosDb.EventStore.EventStreamApi;
 
 public class EventStreamsApiOptions
 {
     public string RoutePrefix { get; set; } = "/event-streams/api";
     public bool EnableCors { get; set; }
-    public string AccessControlAllowMethods { get; set; }
-    public string AccessControlAllowOrigin { get; set; }
-    public string AccessControlAllowCredentials { get; set; }
-    public string AccessControlAllowHeaders { get; set; }
+    public string? AccessControlAllowMethods { get; set; }
+    public string? AccessControlAllowOrigin { get; set; }
+    public string? AccessControlAllowCredentials { get; set; }
+    public string? AccessControlAllowHeaders { get; set; }
 }
