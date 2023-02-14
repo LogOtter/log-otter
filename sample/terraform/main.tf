2f24c7603ae6986ca11e8e36dc846e02259caf4a
Andrew Butler <andrew@atbutler.co.uk>
2023-02-14 12:19:13 +0000

    Use terraform to deploy

---
+++ sample/terraform/main.tf
@@ -0,0 +1,130 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.43.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "Terraform"
+    storage_account_name = "logotterterraformstorage"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  tenant_id       = var.azurerm_tenant_id
+  subscription_id = var.azurerm_subscription_id
+  client_id       = var.azurerm_client_id
+  client_secret   = var.azurerm_client_secret
+
+  features {
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "LogOtter"
+  location = "uksouth"
+}
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "log-otter-samples"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  offer_type          = "Standard"
+  geo_location {
+    location          = azurerm_resource_group.rg.location
+    failover_priority = 0
+  }
+  consistency_policy {
+    consistency_level = "Session"
+  }
+}
+
+resource "azurerm_service_plan" "service-plan" {
+  name                = "log-otter-samples-service-plan"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  os_type             = "Linux"
+  sku_name            = "F1"
+}
+
+resource "azurerm_linux_web_app" "customer-api" {
+  name                = "log-otter-customer-api"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.service-plan.location
+  service_plan_id     = azurerm_service_plan.service-plan.id
+  https_only          = true
+
+  app_settings = {
+    ASPNETCORE_ENVIRONMENT              = "Development"
+    COSMOSDB__CONNECTIONSTRING          = azurerm_cosmosdb_account.db.primary_sql_connection_string
+    DOCKER_REGISTRY_SERVER_URL          = var.docker_registry_url
+    DOCKER_REGISTRY_SERVER_USERNAME     = var.docker_registry_username
+    DOCKER_REGISTRY_SERVER_PASSWORD     = var.docker_registry_password
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
+    WEBSITE_WARMUP_PATH                 = "/health"
+  }
+
+  site_config {
+    always_on     = false
+    http2_enabled = true
+    application_stack {
+      docker_image     = "ghcr.io/logotter/customer-api"
+      docker_image_tag = var.container_tag_name
+    }
+  }
+
+  logs {
+    application_logs {
+      file_system_level = "Information"
+    }
+    http_logs {
+      file_system {
+        retention_in_days = 30
+        retention_in_mb   = 35
+      }
+    }
+  }
+}
+
+resource "azurerm_linux_web_app" "customer-worker" {
+  name                = "log-otter-customer-worker"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.service-plan.location
+  service_plan_id     = azurerm_service_plan.service-plan.id
+  https_only          = true
+
+  app_settings = {
+    DOCKER_REGISTRY_SERVER_URL          = var.docker_registry_url
+    DOCKER_REGISTRY_SERVER_USERNAME     = var.docker_registry_username
+    DOCKER_REGISTRY_SERVER_PASSWORD     = var.docker_registry_password
+    WEBSITES_ENABLE_APP_SERVICE_STORAGE = "false"
+    WEBSITE_WARMUP_PATH                 = "/health"
+  }
+
+  site_config {
+    always_on     = false
+    http2_enabled = true
+    application_stack {
+      docker_image     = "ghcr.io/logotter/customer-worker"
+      docker_image_tag = var.container_tag_name
+    }
+  }
+
+  logs {
+    application_logs {
+      file_system_level = "Information"
+    }
+    http_logs {
+      file_system {
+        retention_in_days = 30
+        retention_in_mb   = 35
+      }
+    }
+  }
+}
