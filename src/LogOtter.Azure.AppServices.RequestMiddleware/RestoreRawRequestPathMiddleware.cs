d6d9261a4105c70dd37cc2ec45636d09424ee9b3
Andrew Butler <1628649+AButler@users.noreply.github.com>
2023-10-12 13:37:44 +0100

    Valid request header matches before rewriting (#190)

--- src/LogOtter.Azure.AppServices.RequestMiddleware/RestoreRawRequestPathMiddleware.cs
+++ src/LogOtter.Azure.AppServices.RequestMiddleware/RestoreRawRequestPathMiddleware.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Net;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
 
 namespace LogOtter.Azure.AppServices.RequestMiddleware;
 
@@ -6,20 +9,52 @@ public class RestoreRawRequestPathMiddleware
 {
     private const string UnencodedUrlHeaderName = "X-Waws-Unencoded-Url";
 
-    private readonly RequestDelegate _next;
+    private static readonly Regex MultipleSlashesRegex = new(@"(\\+)");
 
-    public RestoreRawRequestPathMiddleware(RequestDelegate next)
+    private readonly RequestDelegate _next;
+    private readonly ILogger<RestoreRawRequestPathMiddleware> _logger;
+
+    public RestoreRawRequestPathMiddleware(RequestDelegate next, ILogger<RestoreRawRequestPathMiddleware> logger)
     {
         _next = next;
+        _logger = logger;
     }
 
     public async Task Invoke(HttpContext context)
     {
-        if (context.Request.Headers.TryGetValue(UnencodedUrlHeaderName, out var unencodedUrlValue) && unencodedUrlValue.Any())
+        var request = context.Request;
+
+        if (!request.Headers.TryGetValue(UnencodedUrlHeaderName, out var unencodedUrlHeader) || !unencodedUrlHeader.Any())
         {
-            context.Request.Path = new PathString(unencodedUrlValue.First());
+            await _next(context);
+            return;
         }
 
+        var unencodedPath = unencodedUrlHeader.First();
+        if (!ValidHeader(request.Path, unencodedPath))
+        {
+            _logger.LogWarning(
+                "Mismatch between request path '{RequestPath}' and X-Waws-Unencoded-Url header '{UnencodedHeaderPath}'",
+                request.Path,
+                unencodedPath
+            );
+            await _next(context);
+            return;
+        }
+
+        request.Path = new PathString(unencodedPath);
         await _next(context);
+    }
+
+    private static bool ValidHeader(string pathInUrl, string? unencodedPath)
+    {
+        if (unencodedPath == null)
+        {
+            return false;
+        }
+
+        var encodedPath = Regex.Replace(WebUtility.UrlDecode(unencodedPath), "/+", "/");
+
+        return string.Equals(pathInUrl, encodedPath, StringComparison.Ordinal);
     }
 }
