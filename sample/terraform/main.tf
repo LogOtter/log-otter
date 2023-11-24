bd5d61982c419024868fbf7dee92e82c846c3235
Andrew Butler <1628649+AButler@users.noreply.github.com>
2023-11-24 09:34:56 +0000

    Switch cosmos to free tier (#221)

--- sample/terraform/main.tf
+++ sample/terraform/main.tf
@@ -52,6 +52,10 @@ resource "azurerm_cosmosdb_account" "db" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   offer_type          = "Standard"
+  enable_free_tier    = true
+  capacity {
+    total_throughput_limit = 1000
+  }
   geo_location {
     location          = azurerm_resource_group.rg.location
     failover_priority = 0
