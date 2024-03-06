ea46412daac0aa8bb9df2cf1005dd5631cc62c47
Andrew Butler <1628649+AButler@users.noreply.github.com>
2024-03-06 16:53:46 +0000

    Upgrade npm packages

--- embedded/LogOtter.CosmosDb.EventStore.EventStreamUI/src/main.ts
+++ embedded/LogOtter.CosmosDb.EventStore.EventStreamUI/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import AppContainer from "./AppContainer.vue";
 import "./styles/main.scss";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import * as dayjs from "dayjs";
+import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 
 dayjs.extend(relativeTime);
