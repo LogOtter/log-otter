404d8f502a0c2982c0a4d1226bf4cf78a8b62264
Andrew Butler <1628649+AButler@users.noreply.github.com>
2024-03-10 15:30:54 +0000

    Fix terraform

--- sample/terraform/03-api.tf
+++ sample/terraform/03-api.tf
@@ -3,7 +3,7 @@ resource "azurerm_dns_a_record" "api" {
   zone_name           = azurerm_dns_zone.sample-domain.name
   resource_group_name = azurerm_resource_group.rg.name
   ttl                 = 3600
-  record              = azurerm_container_app_environment.container-app-environment.static_ip_address
+  records             = [azurerm_container_app_environment.container-app-environment.static_ip_address]
 }
 
 resource "azurerm_dns_txt_record" "api" {
